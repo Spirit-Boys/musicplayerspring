9282ea53970567d61940ba4a8f7e8f4d0f82845d
Ghy <63713953+Spirit-Boys@users.noreply.github.com>
2020-06-04 16:13:05 +0800

    添加日志

--- src/main/java/ydhl/eb/ez/api/SongController.java
+++ src/main/java/ydhl/eb/ez/api/SongController.java
@@ -44,17 +44,20 @@ public class SongController {
 	
 	@GetMapping("/random")
 	public Result<List<Song>> getRandom(){
+		logger.info("随机推荐歌曲...");
 		Result result = null;
 		return result;
 	}
 	
 	@GetMapping("/name/{title}")
 	public Result<List<Song>> searchSongByTitle(@PathVariable String title) {
+		logger.info("正在查找指定歌曲名的歌曲信息...");
 		return service.searchSongByTitle(title);
 	}
 
 	@GetMapping("/singer/{quality}")
 	public Result<List<Song>> searchSongBySinger(@PathVariable String singer) {
+		logger.info("正在查找指定歌手的歌曲信息...");
 		return service.searchSongBySinger(singer);
 	}
 }
